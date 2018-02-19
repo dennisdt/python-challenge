556ece9c53f891befcd651bedd4fd694b6bfd5c9
Dennis Tran <dennistran714@gmail.com>
2018-02-19 15:37:50 -0800

    completed homework

--- PyBank/pybank.py
+++ PyBank/pybank.py
@@ -1,1 +1,17 @@
 # PyBank Assignment
+
+import csv
+
+file_path = "raw_data/budget_data_1.csv"
+
+count = 0
+
+with open(file_path) as csv_data:
+    reader = csv.DictReader(csv_data)
+
+    for row in reader:
+        count = count + 1
+        print(row["Date"])
+        print(row['Revenue'])
+
+print("the number of rows is: " + str(count))
